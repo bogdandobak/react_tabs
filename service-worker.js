d545a1fec69ac7022458fa68d3bf77af000b2d93
Bohdan Dobak <chudolipton@gmail.com>
2021-03-11 20:51:11 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.cd9957bdcc2ca13e1a7d51dcb7b8008c.js"
 );
 
 self.addEventListener('message', (event) => {
